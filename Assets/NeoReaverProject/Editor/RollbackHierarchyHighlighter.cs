32637380c8b01fcf717b0b7a2154f5ff05a79ee0
Duncan Bourquard <bourquardduncan@gmail.com>
2020-06-17 13:57:09 +0200

    Creating base rollbacktool to know which entities we need to rollback

---
+++ Assets/NeoReaverProject/Editor/RollbackHierarchyHighlighter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class RollbackHierarchyHighligher {
+
+    List<int> objectsEntitiesToRollback = new List<int>();
+    
+    //==============================================================================
+    //
+    //                                    CONSTANTS
+    //
+    //==============================================================================
+        
+    public static readonly Color DEFAULT_COLOR_HIERARCHY_SELECTED = new Color(0.243f, 0.4901f, 0.9058f, 1f);
+
+    //==============================================================================
+    //
+    //                                    CONSTRUCTORS
+    //
+    //==============================================================================
+
+    static RollbackHierarchyHighligher()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyHighlight_OnGUI;
+    }
+
+    //==============================================================================
+    //
+    //                                    EVENTS
+    //
+    //==============================================================================
+    private static void HierarchyHighlight_OnGUI(int inSelectionID, Rect inSelectionRect) {
+
+        bool mustRollbackObj = RollbackTool.instancesIdList.Contains(inSelectionID);
+
+        GameObject GO_Label = EditorUtility.InstanceIDToObject(inSelectionID) as GameObject;
+
+        if (GO_Label != null) {
+
+            if (mustRollbackObj) {
+                Rect backgroundOffset = new Rect(inSelectionRect.position, inSelectionRect.size);   
+                EditorGUI.DrawRect(backgroundOffset, Color.Lerp(GUI.skin.settings.selectionColor, Color.green, 0.3f));
+            }
+
+            EditorApplication.RepaintHierarchyWindow();
+        }
+    }
+}
---
+++ Assets/NeoReaverProject/Scripts/Tests/Editor/RollbackTool.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RollbackTool : EditorWindow
+{
+    public static List<UnityEngine.Object> rollbackObjectsList = new List<UnityEngine.Object>();
+    public static List<int> instancesIdList = new List<int>();
+
+    bool openedObjectList = false;
+    int currentWantedSize = 0;
+    
+    [MenuItem("RollbackTool/Information")]
+    public static void ShowWindow() {
+        rollbackObjectsList = new List<Object>(); 
+        instancesIdList = new List<int>();
+        GetWindow(typeof(RollbackTool));
+    }
+
+    void OnGUI() {
+        GUILayout.Label("Spawn New Object", EditorStyles.boldLabel);
+        openedObjectList = EditorGUILayout.Foldout(openedObjectList, "Rollback object list");
+        if (openedObjectList) {
+            currentWantedSize += 1;
+            
+            while (currentWantedSize < rollbackObjectsList.Count) {
+                rollbackObjectsList.RemoveAt(rollbackObjectsList.Count - 1);
+                instancesIdList.RemoveAt(instancesIdList.Count - 1);
+            }
+
+            while (currentWantedSize > rollbackObjectsList.Count) {
+                rollbackObjectsList.Add(null);
+                instancesIdList.Add(0);
+            }
+
+            for (int i = 0; i < currentWantedSize; i++) {
+                //Debug.Log("occurence");
+                //Display list of game objects to track rollback on 
+                EditorGUILayout.BeginHorizontal();
+                rollbackObjectsList[i] = EditorGUILayout.ObjectField("object " + i.ToString(), rollbackObjectsList[i], typeof(GameObject), true, GUILayout.ExpandWidth(true)) as GameObject;
+                if (i != currentWantedSize) {
+                    if (GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(20))) {
+                        rollbackObjectsList.RemoveAt(i);
+                        instancesIdList.RemoveAt(i);
+                        currentWantedSize = rollbackObjectsList.Count;
+                        break;
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+                
+                if (rollbackObjectsList[i] != null) {
+                    instancesIdList[i] = rollbackObjectsList[i].GetInstanceID();
+                } else {
+                    currentWantedSize = i;
+                    break;
+                }
+            }
+        }
+    }
+    
+}
