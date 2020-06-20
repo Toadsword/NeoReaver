6d006fbfd5587d179a7963567a8497f69495b405
Duncan Bourquard <bourquardduncan@gmail.com>
2020-06-20 15:12:45 +0200

    Direction and movement of player update with rotation

--- Assets/NeoReaverProject/Scripts/PlayerMovement.cs
+++ Assets/NeoReaverProject/Scripts/PlayerMovement.cs
@@ -4,21 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] float speedo;
+    [SerializeField] float maxSpeedo;
     public Vector2 direction;
-
-    Vector3 fixedLastUpdatePosition;
     
+    Vector3 fixedLastUpdatePosition;
+    float speedo;
+
+    void Start() {
+        
+        speedo = 5.0f;
+    }
+
     void Update() {
         MoveSpaceship(transform.position, Time.deltaTime);
     }
-    
-    void FixedUpdate() {
-        //MoveSpaceship(fixedLastUpdatePosition, Time.fixedDeltaTime);
-        //fixedLastUpdatePosition = transform.position;
-    }
 
     private void MoveSpaceship(Vector3 initPosition, float deltaTime) {
-        transform.position = initPosition + (Vector3)direction * speedo * deltaTime;
+
+        if (direction != Vector2.zero) 
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * 300.0f);
+
+            if (speedo > maxSpeedo) {
+                speedo = maxSpeedo;
+            }
+            if (speedo < 0.0f) {
+                speedo = 0.0f;
+            }
+        }
+
+        float currentAngle = (transform.rotation.eulerAngles.z + 90.0f) * Mathf.Deg2Rad;
+        Vector3 currentDirection = new Vector3(Mathf.Cos(currentAngle) , Mathf.Sin(currentAngle), 0.0f);
+        Debug.Log(currentDirection);
+        transform.position = initPosition + currentDirection * speedo * deltaTime;
     }
 }
