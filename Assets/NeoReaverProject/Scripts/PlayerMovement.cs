ea7f50734ec0013b6f805b56cc9f0ae33a5ab79f
Duncan Bourquard <bourquardduncan@gmail.com>
2020-06-20 15:54:53 +0200

    Fix direction

--- Assets/NeoReaverProject/Scripts/PlayerMovement.cs
+++ Assets/NeoReaverProject/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] float maxSpeedo;
+    [SerializeField] float maxSpeedo = 5.0f;
+    [SerializeField] float minSpeedo = 0.0f;
+    [SerializeField] float speedMultiplier;
     public Vector2 direction;
+
+    float midSpeedo;
     
     Vector3 fixedLastUpdatePosition;
     float speedo;
 
     void Start() {
-        
         speedo = 5.0f;
+        midSpeedo = (maxSpeedo + minSpeedo) / 2.0f;
     }
 
     void Update() {
@@ -24,19 +28,20 @@ public class PlayerMovement : MonoBehaviour {
         if (direction != Vector2.zero) 
         {
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * 300.0f);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * 450.0f);
 
+            
+            
             if (speedo > maxSpeedo) {
                 speedo = maxSpeedo;
             }
-            if (speedo < 0.0f) {
-                speedo = 0.0f;
+            if (speedo < minSpeedo) {
+                speedo = minSpeedo;
             }
         }
 
         float currentAngle = (transform.rotation.eulerAngles.z + 90.0f) * Mathf.Deg2Rad;
         Vector3 currentDirection = new Vector3(Mathf.Cos(currentAngle) , Mathf.Sin(currentAngle), 0.0f);
-        Debug.Log(currentDirection);
         transform.position = initPosition + currentDirection * speedo * deltaTime;
     }
 }
