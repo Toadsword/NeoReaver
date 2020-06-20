6a71faafe78605811c011d05a0de4e3ae2fa730c
Duncan Bourquard <bourquardduncan@gmail.com>
2020-06-20 14:27:56 +0200

    Adding base movements to player

--- Assets/NeoReaverProject/Scripts/PlayerController.cs
+++ Assets/NeoReaverProject/Scripts/PlayerController.cs
@@ -3,32 +3,26 @@ using UnityEngine.Serialization;
 
 namespace NeoReaverProject.Scripts {
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
-
-    [FormerlySerializedAs("speed")] [SerializeField] float _speed = 5.0f;
-    
     private float _horizontal = 0.0f;
     private float _vertical = 0.0f;
-    
 
-    private Rigidbody2D _rigid;
+    PlayerMovement _playerMovement;
+    
     // Start is called before the first frame update
     void Start() {
-        _rigid = GetComponent<Rigidbody2D>();
+        _playerMovement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update() {
         _horizontal = InputActionManager.GetAxis(InputActionManager.AxisType.HORIZONTAL);
         _vertical = InputActionManager.GetAxis(InputActionManager.AxisType.VERTICAL);
-        
-        //Input update
-        _rigid.velocity = new Vector2(_horizontal, _vertical) * _speed;
     }
 
     void FixedUpdate() {
-        
+        //Input update
+        _playerMovement.direction = new Vector2(_horizontal, _vertical);
     }
 }
 }
--- Assets/NeoReaverProject/Scripts/PlayerMovement.cs
+++ Assets/NeoReaverProject/Scripts/PlayerMovement.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] float speedo;
+    public Vector2 direction;
+
+    Vector3 fixedLastUpdatePosition;
+    
+    void Update() {
+        MoveSpaceship(transform.position, Time.deltaTime);
+    }
+    
+    void FixedUpdate() {
+        //MoveSpaceship(fixedLastUpdatePosition, Time.fixedDeltaTime);
+        //fixedLastUpdatePosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void MoveSpaceship(Vector3 initPosition, float deltaTime) {
+        transform.position = initPosition + (Vector3)direction * speedo * deltaTime;
     }
 }
