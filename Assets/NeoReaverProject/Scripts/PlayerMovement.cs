3a040c1cd55cb5d8122f210bfffe2e4712e1196b
Duncan Bourquard <bourquardduncan@gmail.com>
2020-06-22 14:04:59 +0200

    Final player movement v1

--- Assets/NeoReaverProject/Scripts/PlayerMovement.cs
+++ Assets/NeoReaverProject/Scripts/PlayerMovement.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    [Serializable]
+    enum MovementState {
+        IDLE,
+        MOVING,
+        CHANGING_DIRECTION
+    }
+    
     [SerializeField] float maxSpeedo = 5.0f;
-    [SerializeField] float minSpeedo = 0.0f;
+    [SerializeField] float minSpeedo = 0.2f;
     [SerializeField] float speedMultiplier;
+    [SerializeField] float currentSpeedMultiplier;
     public Vector2 direction;
 
-    float midSpeedo;
-    
+
     Vector3 fixedLastUpdatePosition;
-    float speedo;
+    [SerializeField] float midSpeedo;
+    [SerializeField] float currentSpeedo;
+    [SerializeField] MovementState currentMovementState;
 
     void Start() {
-        speedo = 5.0f;
+        currentSpeedo = 5.0f;
         midSpeedo = (maxSpeedo + minSpeedo) / 2.0f;
+        currentSpeedMultiplier = speedMultiplier;
     }
 
     void Update() {
@@ -25,23 +37,49 @@ public class PlayerMovement : MonoBehaviour {
 
     private void MoveSpaceship(Vector3 initPosition, float deltaTime) {
 
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+        float currentAngle = (transform.rotation.eulerAngles.z + 90.0f) * Mathf.Deg2Rad;
+        Quaternion newRotation = transform.rotation;
         if (direction != Vector2.zero) 
         {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * 450.0f);
-
-            
-            
-            if (speedo > maxSpeedo) {
-                speedo = maxSpeedo;
+            newRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * 450.0f);
+            // Calculating new state
+            if (transform.rotation != newRotation) {
+                currentMovementState = MovementState.CHANGING_DIRECTION;
+            } else {
+                currentMovementState = MovementState.MOVING;
             }
-            if (speedo < minSpeedo) {
-                speedo = minSpeedo;
-            }
+        } else {
+            currentMovementState = MovementState.IDLE;
+        }
+        
+        transform.rotation = newRotation;
+        float checkingMaxSpeedo = maxSpeedo;
+        switch (currentMovementState) {
+            case MovementState.IDLE:
+                currentSpeedMultiplier = speedMultiplier;
+                currentSpeedo /= currentSpeedMultiplier;
+                break;
+            case MovementState.MOVING:
+                currentSpeedo *= currentSpeedMultiplier * 5.0f;
+                break;
+            case MovementState.CHANGING_DIRECTION:
+                if (currentAngle - angle > 60.0f) {
+                    currentSpeedMultiplier = speedMultiplier;
+                } else {
+                    currentSpeedo *= currentSpeedMultiplier;
+                } 
+                break;
+        }
+            
+        if (currentSpeedo > checkingMaxSpeedo) {
+            currentSpeedo = checkingMaxSpeedo;
+        }
+        if (currentSpeedo < minSpeedo) {
+            currentSpeedo = minSpeedo;
         }
 
-        float currentAngle = (transform.rotation.eulerAngles.z + 90.0f) * Mathf.Deg2Rad;
         Vector3 currentDirection = new Vector3(Mathf.Cos(currentAngle) , Mathf.Sin(currentAngle), 0.0f);
-        transform.position = initPosition + currentDirection * speedo * deltaTime;
+        transform.position = initPosition + currentDirection * currentSpeedo * deltaTime;
     }
 }
