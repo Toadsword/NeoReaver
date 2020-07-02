5ccb3abf5d82d54a6135d707d644cf3647ab3d97
Duncan Bourquard <bourquardduncan@gmail.com>
2020-07-02 16:05:24 +0200

    Asteroid Field generation done

---
+++ Assets/NeoReaverProject/Scripts/Jam/AsteroidGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidGenerator : MonoBehaviour {
+    [SerializeField] GameObject asteroidPrefab;
+    
+    [SerializeField] float _distanceMinRandom = 1.3f;
+    [SerializeField] float _distanceMaxRandom = 3.0f;
+    [SerializeField] float _distanceDeltaRandom = 0.5f;
+    [SerializeField] float _scaleMinRandom = 1.0f;
+    [SerializeField] float _scaleMaxRandom = 2.0f;
+
+    [SerializeField] int downLeftPos = -200;
+    [SerializeField] int upRightPos = 200;
+
+    void Start() {
+        GenerateAsteroids();
+    }
+
+    private void GenerateAsteroids() {
+        Vector2 currentPos = new Vector2(downLeftPos, downLeftPos);
+        while (currentPos.y < upRightPos) {
+            while (currentPos.x < upRightPos) {
+                currentPos.x += Random.Range(_distanceMinRandom, _distanceMaxRandom);
+                Vector2 tempPos = currentPos;
+                tempPos.x += Random.Range(-_distanceDeltaRandom, _distanceDeltaRandom);
+                tempPos.y += Random.Range(-_distanceDeltaRandom, _distanceDeltaRandom);
+                
+                GameObject obj = Instantiate(asteroidPrefab, (Vector3) tempPos, Quaternion.identity);
+                obj.transform.parent = this.transform;
+                //Apply random scale
+                float randomScale = Random.Range(_scaleMinRandom, _scaleMaxRandom);
+                obj.transform.localScale = new Vector3(randomScale, randomScale, 1.0f);
+            }
+
+            currentPos.y += Random.Range(_distanceMinRandom, _distanceMaxRandom);
+            currentPos.x = downLeftPos + Random.Range(_distanceMinRandom, _distanceMaxRandom);
+        }
+    }
+}
---
+++ Assets/NeoReaverProject/Scripts/Jam/AsteroidMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : IRollbackBehaviour
+{
+    
+    
+    public override void Simulate() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void GoToFrame(int frameNumber) {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DeleteFrames(int fromFrame, int toFrame) {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SaveFrame() {
+        throw new System.NotImplementedException();
+    }
+}
